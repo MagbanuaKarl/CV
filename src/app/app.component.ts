fe05eb46d4f9464dc6762dbb8df5cba91a5b8f00
MagbanuaKarl <100075310+MagbanuaKarl@users.noreply.github.com>
2023-01-24 12:36:56 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Resume';
+  name = 'Magbanua Karl Caniel D';
+  occupation = 'HAU 3rd year college Student';
+  introduction =
+    'I am Magbanua Karl, .Kiks for short a 3rd year college student of Holy Angel University. This website shows my informatios needed for a job application. Everything in this page meet the criteria of the activities. Integrated bootstrap and 5 components namely  About, Education, Skills, Achievements and References';
+
+  about1 =
+    'I have learned simple Java and Python Programming and other Coomputing Fundamentals';
+  about2 =
+    '2nd Year I have improve my coding skills and adding HTML and CSS in my skills while improving general subjects such as Calculus';
+  about3 =
+    'This year I have a little bit of taste on real world works of my future profession and improving other skills that will be needed in my profession';
+
+  school1 =
+    'Humprey Foundation Learning Center from Nursery till I graduate Grade 6';
+  school2 =
+    'I attend Holy Angel University from Junior Highschool and Senior Highschool and still attending this school until I graduate College';
+
+  achievements1 =
+    '1st Quarter of Year 2022 I have achieve Deans Lister for that Semester';
+  achievements2 =
+    'In the following school year 1st semester I have achieve another Deans Lister award in my 3rd year 1st semester';
+
+  relationship = 'College Classmate';
+
+  contact1 = '+63 923 568 0992';
+  contact2 = '+63 934 678 1103';
+  contact3 = '+63 945 910 1114';
+
+  footer =
+    '2023 | 2052 Ilang ilang st. | Brgy. Margot Angeles City | Pampanga | Luzon | Philippines | South East Asia ';
+}
